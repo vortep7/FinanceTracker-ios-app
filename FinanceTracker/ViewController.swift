5542a65eefbe7b1442b67cd6a768d3e418ee8f83
vortep.7 <0ndreyp7777@gmail.com>
2024-03-06 13:49:53 +0300

    Initial Commit

---
+++ FinanceTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinanceTracker
+//
+//  Created by Андрей Петров on 06.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
